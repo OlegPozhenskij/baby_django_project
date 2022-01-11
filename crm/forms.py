5cf8ce8102ca9e196de6b6013bf4e107bffcb8e6
Oleg <pozhenskij2@gmail.com>
2022-01-11 13:07:46 +0400

    get/post requests, forms as classes, validation and template inheritance

--- crm/forms.py
+++ crm/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class OrderForm(forms.Form):
-    name = forms.CharField(max_length=280)
+    name = forms.CharField(max_length=280, required=False)
     phone = forms.CharField(max_length=280)
