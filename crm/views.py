db649908486eb66914fadbb1664d92fc117d5633
Oleg <pozhenskij2@gmail.com>
2022-01-12 12:08:26 +0400

    create app "price"

--- crm/views.py
+++ crm/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from .models import Order
 from .forms import OrderForm
 from cms.models import CmsSlider
+from price.models import PriceCard, PriceTable
 
 
 def first_page(request):
     slider_list = CmsSlider.objects.all() #get all objects from db
+    pc_1 = PriceCard.objects.get(pk=1)
+    pc_2 = PriceCard.objects.get(pk=2)
+    pc_3 = PriceCard.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+
+    dict_obj = {'slider_list': slider_list,
+                'pc_1': pc_1, 'pc_2': pc_2,
+                'pc_3': pc_3, 'price_table': price_table}
+
     '''Указываем параметры подачи в шаблон с помощью словаря'''
-    return render(request, './index.html', {'slider_list': slider_list})
+    return render(request, './index.html', dict_obj)
 
 
 def thanks_page(request):
