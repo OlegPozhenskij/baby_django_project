b7bd02f5f882a8aa4b64f808f59ae45f341b7922
Oleg <pozhenskij2@gmail.com>
2022-01-13 17:19:42 +0400

    customize admin panel

--- cms/admin.py
+++ cms/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import CmsSlider
 
 # Register your models here.
-admin.site.register(CmsSlider)
+class CmsAdmin(admin.ModelAdmin):
+    list_display = ('cms_title', 'cms_text', 'cms_css', 'get_img')
+    list_display_links = ('cms_title',)
+    list_editable = ('cms_css',)
+    fields = ('cms_title', 'cms_text', 'cms_css', 'cms_img', 'get_image')
+    readonly_fields = ('get_image',)
+
+    def get_img(self, obj):
+        if obj.cms_img:
+            # mark_safe() - помечает строку как код, а не как текст
+            return mark_safe(f'<img src="{obj.cms_img.url}" width="80px">')
+        else:
+            return 'Нет картинки!'
+
+    get_img.short_description = 'Миниатюра'
+
+
+admin.site.register(CmsSlider, CmsAdmin)
--- crm/admin.py
+++ crm/admin.py
@@ -2,6 +2,31 @@ from django.contrib import admin
 from .models import Order, StatusName, CommentCrm
 
 # Register your models here.
-admin.site.register(Order)
+
+class Comment(admin.StackedInline):
+    model = CommentCrm
+    fields = ('comment_dt', 'comment_text')
+    readonly_fields = ('comment_dt',)
+    extra = 0
+
+
+# настройка отображения элементов в админ панели
+class OrderAdm(admin.ModelAdmin):
+    list_display = ('id', 'order_status', 'order_name', 'order_phone', 'order_dt')
+    list_display_links = ('id', 'order_name')
+    search_fields = ('id', 'order_name', 'order_phone', 'order_dt')
+    list_filter = ('order_status',)
+    list_editable = ('order_status', 'order_phone')
+    list_per_page = 10
+    list_max_show_all = 100
+
+    fields = ('id', 'order_status', 'order_dt', 'order_name', 'order_phone')
+    readonly_fields = ('id', 'order_dt',)
+
+    #поле класса коммент
+    inlines = [Comment,]
+
+
+admin.site.register(Order, OrderAdm)
 admin.site.register(StatusName)
 admin.site.register(CommentCrm)
